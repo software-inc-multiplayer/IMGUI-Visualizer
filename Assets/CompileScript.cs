2a126bea0fed396aee2596f95abf5cca99c0b507
CyaCal <calumproh28@gmail.com>
2020-09-01 11:00:37 +0100

    Added Button + export

--- Assets/CompileScript.cs
+++ Assets/CompileScript.cs
@@ -5,6 +5,9 @@ using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using SimpleFileBrowser;
+using System.Text;
+using System.Collections;
 
 namespace SMMMGUIEdior
 {
@@ -23,55 +26,64 @@ namespace SMMMGUIEdior
         public Button ExportButton;
 
         public List<Action> actions = new List<Action>();
-        public bool CanBeCompiled { get; set; }
         void OnGUI()
         {
-            foreach (Action acc in actions) acc();
-            if (CanBeCompiled)
-            {
-                CanBeCompiled = false;
-                var script = CodeBox.text.Split("\n".ToCharArray()).OfType<string>().ToList();
-                script.ForEach((string command) =>
-                {
-                    Regex rectEx = new Regex("(([0-9]*), ([0-9]*), ([0-9]*), ([0-9]*))");
-                    Match rectMatch = rectEx.Match(command);
-                    Regex commandEx = new Regex("^([a-zA-Z]*)");
-                    Match commandMatch = commandEx.Match(command);
-                    Regex stringEx = new Regex("\"(.*?)\"");
-                    Match stringMatch = stringEx.Match(command);
-                    List<string> rects = rectMatch.Value.Split(",".ToCharArray()).OfType<string>().ToList();
-                    List<float> parsedRects = new List<float>();
-                    switch (commandMatch.Value)
-                    {
-                        case "Label":
-                            
-                            foreach (string rect in rects)
-                            {
-                                parsedRects.Add(float.Parse(rect.Replace(",", "").Trim()));
-                            }
-                            actions.Add(() => GUI.Label(new Rect(parsedRects[0], parsedRects[1], parsedRects[2], parsedRects[3]), stringMatch.Value.RemoveQuotes()));
-                            break;
-                        case "Box":
-                            foreach (string rect in rects)
-                            {
-                                parsedRects.Add(float.Parse(rect.Replace(",", "").Trim()));
-                            }
-                            actions.Add(() => GUI.Box(new Rect(parsedRects[0], parsedRects[1], parsedRects[2], parsedRects[3]), stringMatch.Value.RemoveQuotes()));
-                            break;
-                    }
-                });
-            }
+            foreach (Action acc in actions) acc();               
         }
         void Start()
-        {
-            
-            CanBeCompiled = false;
+        {           
             CompileButton.onClick.AddListener(CompileTScript);
             ClearUIButton.onClick.AddListener(() => actions.Clear());
+            ExportButton.onClick.AddListener(Export);
+            FileBrowser.SetFilters(true, new FileBrowser.Filter("IMGUI File", ".imgui"));
+            FileBrowser.SetDefaultFilter(".imgui");
         }
+        public void Export()
+        {
+            StartCoroutine(ShowLoadDialogCoroutine());
+        }
+
+        IEnumerator ShowLoadDialogCoroutine()
+        {
+            yield return FileBrowser.WaitForSaveDialog(false, false, null, "Export Script", "Export");
+            Debug.Log(FileBrowser.Success);
+            if (FileBrowser.Success)
+            {
+                FileBrowserHelpers.WriteBytesToFile(FileBrowser.Result[0], Encoding.UTF8.GetBytes(CodeBox.text));
+            }
+        }
+
         public void CompileTScript()
         {
-            CanBeCompiled = true;
+            var script = CodeBox.text.Split("\n".ToCharArray()).OfType<string>().ToList();
+            script.ForEach((string command) =>
+            {
+
+                Regex commandEx = new Regex("^([a-zA-Z]*)");
+                Match commandMatch = commandEx.Match(command);
+                Regex rectEx = new Regex("(([0-9]*), ([0-9]*), ([0-9]*), ([0-9]*))");
+                Match rectMatch = rectEx.Match(command);
+                List<string> rects = rectMatch.Value.Split(",".ToCharArray()).OfType<string>().ToList();
+                List<float> parsedRects = new List<float>();
+                Regex stringEx = new Regex("\"(.*?)\"");
+                Match stringMatch = stringEx.Match(command);
+                foreach (string rect in rects)
+                {
+                    parsedRects.Add(float.Parse(rect.Replace(",", "").Trim()));
+                }
+                switch (commandMatch.Value)
+                {
+                    case "Label":
+                        actions.Add(() => GUI.Label(new Rect(parsedRects[0], parsedRects[1], parsedRects[2], parsedRects[3]), stringMatch.Value.RemoveQuotes()));
+                        break;
+                    case "Box":
+                        actions.Add(() => GUI.Box(new Rect(parsedRects[0], parsedRects[1], parsedRects[2], parsedRects[3]), stringMatch.Value.RemoveQuotes()));
+                        break;
+                    case "Button":
+                        actions.Add(() => GUI.Button(new Rect(parsedRects[0], parsedRects[1], parsedRects[2], parsedRects[3]), stringMatch.Value.RemoveQuotes()));
+                        break;
+                }
+            });
         }
     }
 }
